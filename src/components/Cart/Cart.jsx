e6332e7cb95849c320ff1dd602b1f792682b119a
Truong Tan Dat <truongtandat1409@gmail.com>
2022-10-13 10:52:28 +0700

    fix checkout

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,18 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../Context/CartContext";
-import "./cart.css" 
+import "./cart.css"
 const Cart = () => {
-
-    const {myCart, total} = useContext(CartContext);
-    return ( 
+    const navigate = useNavigate();
+    const { myCart, total, addToCart, setTotal } = useContext(CartContext);
+    const handleCheckout = () => {
+        setTotal(0);
+        addToCart([]);
+    }
+    const handleHome = () => {
+        navigate("/")
+    }
+    return (
         <>
             <section className="cart__container">
                 <div className="cart__header">
@@ -12,21 +20,30 @@ const Cart = () => {
                 </div>
                 <div className="cart__items">
                     {myCart.map((item) => {
-                        
+
                         return (
                             <div className="cart__item" >
                                 <img className="cart__img" src={item.image} alt=""></img>
                                 <div className="cart__text">{item.name} : {item.price}$</div>
-                                
+
                             </div>
                         )
-                       
+
                     })}
+                    <div className="cart__payment">Total: {total}$</div>
+
                 </div>
-                <div className="cart__payment">Total: {total}$</div>
+                <div className="cart__button_checkout">
+                    <button className="cart__checkout" onClick={handleCheckout}>DONE</button>
+                </div>
+                <div className="cart__button_gohome">
+                    <button className="cart__gohome" onClick={handleHome}>Go back home</button>
+
+                </div>
+
             </section>
         </>
-     );
+    );
 }
- 
+
 export default Cart;
